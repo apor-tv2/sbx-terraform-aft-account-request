b2ceecb00a20987952bc6108f94bdd958bc257dc
Anders Porsbo <apor@tv2.dk>
2024-04-23 10:39:03 +0200

    removed test enrollment again

--- terraform/TestEnrollment.tf
+++ terraform/TestEnrollment.tf
@@ -1,27 +1,27 @@
-module "TestEnrollment1" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "apor+testenrollment1@tv2.dk"
-    AccountName               = "testenrollment1"
-    ManagedOrganizationalUnit = "sandbox"
-    SSOUserEmail              = "apor+enrollment1@tv2.dk"
-    SSOUserFirstName          = "Anders"
-    SSOUserLastName           = "Porsbo"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "testenrollment1"
-}
+#module "TestEnrollment1" {
+#  source = "./modules/aft-account-request"
+#
+#  control_tower_parameters = {
+#    AccountEmail              = "apor+testenrollment1@tv2.dk"
+#    AccountName               = "testenrollment1"
+#    ManagedOrganizationalUnit = "sandbox"
+#    SSOUserEmail              = "apor+enrollment1@tv2.dk"
+#    SSOUserFirstName          = "Anders"
+#    SSOUserLastName           = "Porsbo"
+#  }
+#
+#  account_tags = {
+#    "Learn Tutorial" = "AFT"
+#  }
+#
+#  change_management_parameters = {
+#    change_requested_by = "HashiCorp Learn"
+#    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#  }
+#
+#  custom_fields = {
+#    group = "non-prod"
+#  }
+#
+#  account_customizations_name = "testenrollment1"
+#}
