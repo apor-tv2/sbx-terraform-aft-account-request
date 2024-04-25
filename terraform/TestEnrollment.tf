73045b6fb378c6cf21acf1cb1f7fe88e7991ed94
Anders Porsbo <apor@tv2.dk>
2024-04-25 09:27:01 +0200

    removed testenrollment 1 and 2, but added 3

--- terraform/TestEnrollment.tf
+++ terraform/TestEnrollment.tf
@@ -1,11 +1,11 @@
-module "TestEnrollment1" {
+module "TestEnrollment3" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "apor+testenrollment1@tv2.dk"
-    AccountName               = "testenrollment1"
+    AccountEmail              = "apor+testenrollment3@tv2.dk"
+    AccountName               = "testenrollment3"
     ManagedOrganizationalUnit = "sandbox"
-    SSOUserEmail              = "apor+enrollment1@tv2.dk"
+    SSOUserEmail              = "apor+testenrollment3@tv2.dk"
     SSOUserFirstName          = "Anders"
     SSOUserLastName           = "Porsbo"
   }
@@ -22,33 +22,5 @@ module "TestEnrollment1" {
   custom_fields = {
     group = "non-prod"
   }
-
-#  account_customizations_name = "testenrollment1"
-}
-module "TestEnrollment2" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "apor+testenrollment2@tv2.dk"
-    AccountName               = "testenrollment2"
-    ManagedOrganizationalUnit = "sandbox"
-    SSOUserEmail              = "apor+enrollment2@tv2.dk"
-    SSOUserFirstName          = "Anders"
-    SSOUserLastName           = "Porsbo"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "testenrollment2"
+  account_customizations_name = "testenrollment3"
 }
