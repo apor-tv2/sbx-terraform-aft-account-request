50ba906256b5dea569a2cfad6e84c5e940bec46b
Anders Porsbo <apor@tv2.dk>
2024-05-13 10:42:51 +0200

    added account tags for cost allocation

--- terraform/control-tower-management.tf
+++ terraform/control-tower-management.tf
@@ -11,12 +11,14 @@ module "ControlTowerManagement" {
   }
 
   account_tags = {
-    "SBX Learning" = "AFT"
+    "CostCenterNumber" = "4693"
+    "CostCenterBrief" = "DCP"
+    "Usage" = "ControlTower Management"
   }
 
   change_management_parameters = {
-    change_requested_by = "apor testing scp with aft"
-    change_reason       = "Roll SCPs on Control Tower Management Account"
+    change_requested_by = "APOR"
+    change_reason       = "APOR investigating account tagging for cost reports"
   }
 
   custom_fields = {
